ce0f759312f4e5c87f5d1d600c8dc444ac9ed24a
Ewantha <ewantha@outlook.com>
2023-11-16 22:07:17 +0530

    Improve Open API config

--- src/main/java/com/eudagama12/example/pdfgenerator/config/OpenAPIConfig.java
+++ src/main/java/com/eudagama12/example/pdfgenerator/config/OpenAPIConfig.java
@@ -4,11 +4,18 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Open API Configuration class.
+ */
 @Configuration
 public class OpenAPIConfig {
+
+    @Value("${build.version}")  // Get project version as a value via application.properties file
+    private String version;
 
     @Bean
     public OpenAPI springOpenAPI() {
@@ -16,7 +23,7 @@ public class OpenAPIConfig {
                 .info(new Info().title("PDF Generator")
                         .description("Application to create a PDF using an API")
                         .contact(new Contact().name("Ewantha Udagama").email("ewanthau@gmail.com").url("https://github.com/Ewantha"))
-                        .version("v0.0.1"))
+                        .version(version))
                 .externalDocs(new ExternalDocumentation()
                         .description("itext Website")
                         .url("https://itextpdf.com/"));
