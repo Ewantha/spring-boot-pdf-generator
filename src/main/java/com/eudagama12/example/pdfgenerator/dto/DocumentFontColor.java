7c8acc7ca11cc69dce7a591cbb6ddc2346fcb681
Ewantha <ewantha@outlook.com>
2023-11-17 21:28:56 +0530

    Add documentation

--- src/main/java/com/eudagama12/example/pdfgenerator/dto/DocumentFontColor.java
+++ src/main/java/com/eudagama12/example/pdfgenerator/dto/DocumentFontColor.java
@@ -1,6 +1,7 @@
 package com.eudagama12.example.pdfgenerator.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,10 +10,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DocumentFontColor {
+    @Schema(defaultValue = "0", minimum = "0", maximum = "255")
     @JsonProperty(value = "Red")
     int red;
+    @Schema(defaultValue = "0", minimum = "0", maximum = "255")
     @JsonProperty(value = "Green")
     int green;
+    @Schema(defaultValue = "0", minimum = "0", maximum = "255")
     @JsonProperty(value = "Blue")
     int blue;
 }
--- src/main/java/com/eudagama12/example/pdfgenerator/dto/ElementFont.java
+++ src/main/java/com/eudagama12/example/pdfgenerator/dto/ElementFont.java
@@ -1,6 +1,8 @@
 package com.eudagama12.example.pdfgenerator.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.itextpdf.text.FontFactory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,12 +11,18 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ElementFont {
+    @Schema(description = "Font type", allowableValues = {FontFactory.COURIER, FontFactory.COURIER_BOLD, FontFactory.COURIER_OBLIQUE, FontFactory.COURIER_BOLDOBLIQUE,
+            FontFactory.HELVETICA, FontFactory.HELVETICA_BOLD, FontFactory.HELVETICA_OBLIQUE, FontFactory.HELVETICA_BOLDOBLIQUE, FontFactory.SYMBOL,
+            FontFactory.TIMES, FontFactory.TIMES_ROMAN, FontFactory.TIMES_BOLD, FontFactory.TIMES_ITALIC, FontFactory.TIMES_BOLDITALIC, FontFactory.ZAPFDINGBATS},
+    defaultValue = FontFactory.HELVETICA)
     @JsonProperty(value = "Type")
     private String fontType;
 
+    @Schema(description = "Font size", defaultValue = "12")
     @JsonProperty(value = "Size")
     private int fontSize;
 
+    @Schema(description = "Font style. Normal:0, Bold:1, Italic:2, Underline:4, Strikethrough:8, Bold-Italic:3", defaultValue = "0")
     @JsonProperty(value = "Style")
     private int fontStyle;
 
